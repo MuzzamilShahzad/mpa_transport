8eb2611a425fe6b7ce4084e02ab28723d7802602
Muzzamil <muzzamil.shahzad@gmail.com>
2022-05-12 10:58:49 +0500

    Updated Student Vehicle table

--- database/migrations/2022_04_21_072056_create_student_vehicle_table.php
+++ database/migrations/2022_04_21_072056_create_student_vehicle_table.php
@@ -17,11 +17,20 @@ return new class extends Migration
             
             $table->increments('id');
 
-            $table->unsignedInteger('student_id');
-            $table->foreign('student_id')->references('id')->on('students');
-            
+            $table->unsignedInteger('driver_id');
+            $table->foreign('driver_id')->references('id')->on('drivers');
+
             $table->unsignedInteger('vehicle_id');
             $table->foreign('vehicle_id')->references('id')->on('vehicles');
+
+            $table->unsignedInteger('shift_id');
+            $table->foreign('shift_id')->references('id')->on('shifts');
+
+            $table->unsignedInteger('route_id');
+            $table->foreign('route_id')->references('id')->on('routes');
+
+            $table->unsignedInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students');
             
             $table->date('joining_date');
             $table->tinyInteger('is_active')->default(1);
